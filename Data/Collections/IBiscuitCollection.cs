054774813e399dcbee9bcd023796efae09698225
patrick howell <patrick.howell@outlook.com>
2018-01-28 11:54:35 +0000

    Add stub biscuit collection

---
+++ Data/Collections/IBiscuitCollection.cs
@@ -0,0 +1,18 @@
+﻿using Data.Dtos;
+using System.Collections.Generic;
+
+namespace Data.Collections
+{
+    public interface IBiscuitCollection
+    {
+        List<BiscuitDto> GetAllBiscuits();
+
+        BiscuitDto GetBiscuitById(int biscuitId);
+
+        BiscuitDto AddBiscuit(BiscuitDto);
+
+        BiscuitDto UpdateBiscuit(BiscuitDto);
+
+        bool DeleteBiscuit(int id);
+    }
+}
